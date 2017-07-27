e99c59ccdeb5e3504ec71fb023ed10ac2ef34fbf
Maksim Ovcharik <maksim.ovcharik@gmail.com>
2017-07-27 19:24:18 +0300

    Initial configuration

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import './styles/index.less';
+
+import _ from 'underscore';
+
+console.log('Hello, world!');
+console.log(_);
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+    index: [
+      path.resolve(__dirname, 'src/templates/index.pug'),
+    ],
+    app: [
+      path.resolve(__dirname, 'src/app.js'),
+    ],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(pug|jade)$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {
+              loader: 'html-loader'
+            },
+            {
+              loader: 'pug-html-loader',
+              options: { pretty: true, exports: false },
+            },
+          ],
+        }),
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules\/*/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            ['es2015', { modules: false }],
+          ],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new ExtractTextPlugin({
+      filename: '[name].html',
+    }),
+  ],
+  devServer: {
+    contentBase: path.resolve(__dirname, './src'),
+  },
+}
